e2bbc623ad022fb05214fa7726cb7baa51288107
Chris Odicho <chris.odicho@gmail.com>
2016-08-03 14:45:18 +1000

    Class: ToyRobotSim::Table passing tests

--- lib/toy_robot_sim/table.rb
+++ lib/toy_robot_sim/table.rb
@@ -1,3 +1,24 @@
 class ToyRobotSim::Table
 
+  def initialize(width, height)
+    @width = width
+    @height = height
+  end
+
+  def width
+    @width ||= self.width
+  end
+
+  def height
+    @height ||= self.height
+  end
+
+  def width_in_range?(x)
+    x >= 0 && x <= (width - 1)
+  end
+
+  def height_in_range?(y)
+    y >= 0 && y <= (height - 1)
+  end
+
 end
--- spec/toy_robot_sim/table_spec.rb
+++ spec/toy_robot_sim/table_spec.rb
@@ -1,6 +1,18 @@
 require 'spec_helper'
 
 describe ToyRobotSim::Table do
-  it 'has a width and height'
-  it 'prevents objects from being outside the width and height'
+  let(:table) { ToyRobotSim::Table.new(5,5) }
+
+  it 'has a width and height' do
+    expect(table.width).to eq(5)
+    expect(table.height).to eq(5)
+  end
+
+  it 'prevents objects from being outside the width and height' do
+    expect(table.width_in_range?(10)).to be false
+    expect(table.height_in_range?(10)).to be false
+
+    expect(table.width_in_range?(5)).to be false
+    expect(table.height_in_range?(5)).to be false
+  end
 end
