d7ffb341064738b22b3aadf4d2e5cc6e2395c233
Chris Odicho <chris.odicho@gmail.com>
2016-08-03 14:13:54 +1000

    initial commit

---
+++ lib/toy_robot_sim.rb
@@ -0,0 +1,5 @@
+require "toy_robot_sim/version"
+
+module ToyRobotSim
+  # Your code goes here...
+end
---
+++ spec/toy_robot_sim_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe ToyRobotSim do
+  it 'has a version number' do
+    expect(ToyRobotSim::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
